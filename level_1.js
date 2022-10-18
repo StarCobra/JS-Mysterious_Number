17c270efa89414e39d19526611012511412e1d24
Vigelos0 <akizsmar@gmail.com>
2022-10-18 22:46:15 +0200

    feature: génération de nombre aléatoire.

---
+++ level_1.js
@@ -0,0 +1,31 @@
+function getRandomInt(max) {
+    return Math.floor((Math.random() * max)+1);
+}
+
+random = getRandomInt(98);
+
+nbPropose = document.querySelector("#input");
+
+function getValue() {
+    let value = nbPropose.value;
+    console.log(value);
+    return value;
+}
+
+result = document.querySelector("#response");
+
+function Verification() {
+    let verifNumber = getValue();
+    if((verifNumber < 100) && (verifNumber > 0)) {
+        if(verifNumber > random) {
+            result.innerHTML = verifNumber+" est supérieur au nombre Mystère !";
+        } else if(verifNumber < random) {
+            result.innerHTML = verifNumber+" est inférieur au nombre Mystère !";
+        } else {
+            result.innerHTML = "Félicitations ! Vous avez trouvé le nombre Mystère qui était :"+random;
+        }
+    } else {
+        alert("Votre nombre n'est pas compris entre 1 et 99.")
+    }
+    nbPropose.value = "";
+}
