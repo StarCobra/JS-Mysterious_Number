572ba0686c89883f6197ed6cd25097c1d587238d
Vigelos0 <akizsmar@gmail.com>
2022-10-21 23:46:58 +0200

    feature: jointure entre html/css et javascript

--- level_1.js
+++ level_1.js
@@ -2,19 +2,92 @@
 
 import { SaveClassement, LoadJoueur, LoadIndex, LoadClassement, Clear, Order, Score, OrderClassement } from "./System_Stockage.js";
 
-document.querySelector(".pseudo").style.display = "none";
-
 function getRandomInt(max) {
     return Math.floor((Math.random() * max) + 1);
 }
 
 let nbEssais = 0;
-//let nbEssaisMax = 20;
-let difficulty = 3;
+let nbEssaisMax;
 let pseudo;
+let easy;
+let medium;
+let hard;
+let impossible;
+let alert = 99;
+let playNumber = document.querySelector("#playNumber");
+
+playNumber.innerHTML = "Saisissez un nombre entre 0 et 99.";
+
+function getEasy() {
+    easy = document.querySelector("#easy").value;
+    nbEssaisMax = 40;
+    document.querySelector("#essaiRestant").innerHTML = (nbEssaisMax - nbEssais);
+    return easy;
+}
+
+const inputEasy = document.querySelector("#easy");
+inputEasy.addEventListener("click", function (event) {
+    getEasy();
+    let last = document.querySelector("#iDifficult");
+    last.style.display = "none";
+    let news = document.querySelector("#iPlay");
+    news.style.display = "block";
+});
+
+function getMedium() {
+    medium = document.querySelector("#medium").value;
+    nbEssaisMax = 20;
+    document.querySelector("#essaiRestant").innerHTML = (nbEssaisMax - nbEssais);
+    return medium;
+}
+
+const inputMedium = document.querySelector("#medium");
+inputMedium.addEventListener("click", function (event) {
+    getMedium();
+    let last = document.querySelector("#iDifficult");
+    last.style.display = "none";
+    let news = document.querySelector("#iPlay");
+    news.style.display = "block";
+});
+
+function getHard() {
+    hard = document.querySelector("#hard").value;
+    nbEssaisMax = 10;
+    document.querySelector("#essaiRestant").innerHTML = (nbEssaisMax - nbEssais);
+    return hard;
+}
+
+const inputHard = document.querySelector("#hard");
+inputHard.addEventListener("click", function (event) {
+    getHard();
+    let last = document.querySelector("#iDifficult");
+    last.style.display = "none";
+    let news = document.querySelector("#iPlay");
+    news.style.display = "block";
+});
+
+function getImpossible() {
+    impossible = document.querySelector("#impossible").value;
+    nbEssaisMax = 15;
+    document.querySelector("#essaiRestant").innerHTML = (nbEssaisMax - nbEssais);
+    alert = 999;
+    random = getRandomInt(998);
+    return impossible;
+}
+
+const inputImpossible = document.querySelector("#impossible");
+inputImpossible.addEventListener("click", function (event) {
+    getImpossible();
+    let last = document.querySelector("#iDifficult");
+    last.style.display = "none";
+    let news = document.querySelector("#iPlay");
+    news.style.display = "block";
+    playNumber.innerHTML = "Saisissez un nombre entre 0 et 999.";
+});
+
+
 
 export let random = getRandomInt(98);
-document.querySelector("#cheat").innerHTML = random; //A suppr
 
 function getValue() {
     return document.querySelector("#input").value;
@@ -37,9 +110,13 @@ function Verification() {
             ButtonPropose.disabled = true;
             document.querySelector(".pseudo").style.display = "block";
         }
+    } else if(nbEssais >= nbEssaisMax){
+        document.querySelector("#response").innerHTML = "Désolé ! Vous avez utilisé vos "+nbEssaisMax+" essais.<br>Vous n'avez pas trouvé le nombre Mystère qui était : " + random + ".";
     } else {
-        alert("Votre nombre n'est pas compris entre 1 et 99.")
+        alert("Votre nombre n'est pas compris entre 1 et "+alert+".");
     }
+    document.querySelector("#essaiRestant").innerHTML = (nbEssaisMax - nbEssais);
+
     document.querySelector("#input").value = "";
 }
 
