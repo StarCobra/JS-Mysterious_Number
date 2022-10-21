838d3133ef5c81d89204c4e9b0e4c5ea2a1b8e3c
Vigelos0 <akizsmar@gmail.com>
2022-10-21 15:09:42 +0200

    feature: mise a jour et correction du niveau 1.

--- System_Stockage.js
+++ System_Stockage.js
@@ -1,17 +1,17 @@
-function SaveClassement(Pseudo,NbEssais,Difficulté=3) {
+export function SaveClassement(Pseudo,NbEssais,Difficulté=3) {
 	var Data = "NumMyst|"+Pseudo+"|"+Difficulté+"|"+NbEssais;
 	localStorage.setItem(Pseudo,Data);
 }
 
-function LoadJoueur(Pseudo) {
+export function LoadJoueur(Pseudo) {
 	return localStorage.getItem(Pseudo);
 }
 
-function LoadIndex(Index) {
+export function LoadIndex(Index) {
 	return LoadJoueur(localStorage.key(Index));
 }
 
-function LoadClassement() {
+export function LoadClassement() {
 	var Classement = [];
 	var y = 0;
 	for(var i=0;i<localStorage.length;i++) {
@@ -23,22 +23,22 @@ function LoadClassement() {
 	return Classement;
 }
 
-function Clear() {
+export function Clear() {
 		localStorage.clear();
 }
 
-function Order(a,b) {
+export function Order(a,b) {
 	// Test de calcul de score
 	//console.log("score a ("+a[1]+") = "+(((20-a[3])*a[2])));
 	//console.log("score b ("+b[1]+") = "+((20-b[3])*b[2]));
 	return Score(a) - Score(b);
 }
 
-function Score(a) {
+export function Score(a) {
 	return ((20-a[3])*a[2]);
 }
 
 //Rend les joueurs par ordre de classement (indice 0 = plus faible; indice 1 < indice 2)
-function OrderClassement(Classement) {
+export function OrderClassement(Classement) {
 	return Classement.sort((a, b) => Order(a,b));
 }
--- level_1.js
+++ level_1.js
@@ -1,10 +1,14 @@
+'use_strict';
+
+import { SaveClassement, LoadJoueur, LoadIndex, LoadClassement, Clear, Order, Score, OrderClassement } from "./System_Stockage.js";
+
 function getRandomInt(max) {
     return Math.floor((Math.random() * max)+1);
 }
 
-random = getRandomInt(98);
+let random = getRandomInt(98);
 
-nbPropose = document.querySelector("#input");
+let nbPropose = document.querySelector("#input");
 
 function getValue() {
     let value = nbPropose.value;
@@ -12,20 +16,51 @@ function getValue() {
     return value;
 }
 
-result = document.querySelector("#response");
+let nbEssais = 0;
+let nbEssaisMax;
+let difficulty = 3;
+
+let result = document.querySelector("#response");
+let cheat = document.querySelector("#cheat");
+
+cheat.innerHTML = random;
 
 function Verification() {
     let verifNumber = getValue();
     if((verifNumber < 100) && (verifNumber > 0)) {
+        nbEssais++;
         if(verifNumber > random) {
             result.innerHTML = verifNumber+" est supérieur au nombre Mystère !";
         } else if(verifNumber < random) {
             result.innerHTML = verifNumber+" est inférieur au nombre Mystère !";
         } else {
-            result.innerHTML = "Félicitations ! Vous avez trouvé le nombre Mystère qui était :"+random;
+            result.innerHTML = "Félicitations ! Vous avez trouvé le nombre Mystère qui était : "+random+".";
+            result.innerHTML += "<br>Vous avez trouvé en "+nbEssais+" essais !";
+            Victore();
         }
     } else {
         alert("Votre nombre n'est pas compris entre 1 et 99.")
     }
     nbPropose.value = "";
+}   
+
+const input = document.querySelector("#input");
+input.addEventListener("keyup", function(event) {
+    if (event.key === "Enter" || event.key === 13) {
+       return Verification();
+    }
+});
+
+function getPseudo() {
+    let pseudo = document.querySelector('#input-pseudo');
+    let value = pseudo.value;
+    console.log(value);
+    return value;
+}
+
+const valuepseudo = getPseudo();
+
+function Victore() {
+    result.innerHTML += "<br><br><h4>Veuillez inscrire votre pseudo :</h4><input type='text' id='input-pseudo' placeholder='votre pseudo'>";
+    result.innerHTML += "<button id='button-pseudo' onclick='SaveClassement("+valuepseudo+","+nbEssais+","+difficulty+");'>Valider !</button>";
 }
