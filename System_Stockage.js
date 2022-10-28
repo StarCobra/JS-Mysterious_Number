9235fae240a72132671d197925797a1599b18c50
StrixOfTheWeb <78547521+StrixOfTheWeb@users.noreply.github.com>
2022-10-28 08:53:39 +0200

    Update de SaveClassement
    La fonction SaveClassement inclut désormais une sécurité pour éviter les duplicata de pseudo et l'écrasement de donnée qui s'en suit : quand un pseudo existe déjà, la fonction n'enregistre pas et revoit FALSE. Si il n'y a pas de duplicata, l'enregistrement se fera sans problème et la fonction renvera TRUE.

--- System_Stockage.js
+++ System_Stockage.js
@@ -1,44 +1,52 @@
-function SaveClassement(Pseudo,NbEssais,Difficulté=3) {
-	var Data = "NumMyst|"+Pseudo+"|"+Difficulté+"|"+NbEssais;
-	localStorage.setItem(Pseudo,Data);
+import { random } from './level_1.js';
+
+export function SaveClassement(Pseudo, NbEssais, difficulty) {
+	if(!difficulty) {difficulty = 2;}
+	if (LoadJoueur(Pseudo) == null) {
+		var Data = random + "|" + Pseudo + "|" + NbEssais + "|" + difficulty;
+		localStorage.setItem(Pseudo, Data);
+		return true;
+	} else {
+		return false;
+	}
 }
 
-function LoadJoueur(Pseudo) {
+export function LoadJoueur(Pseudo) {
 	return localStorage.getItem(Pseudo);
 }
 
-function LoadIndex(Index) {
+export function LoadIndex(Index) {
 	return LoadJoueur(localStorage.key(Index));
 }
 
-function LoadClassement() {
+export function LoadClassement() {
 	var Classement = [];
 	var y = 0;
-	for(var i=0;i<localStorage.length;i++) {
+	for (var i = 0; i < localStorage.length; i++) {
 		var temp = LoadIndex(i).split("|");
-		if(typeof temp[3] !== 'undefined') {
+		if (typeof temp[3] !== 'undefined') {
 			Classement[y++] = temp;
 		}
 	}
 	return Classement;
 }
 
-function Clear() {
-		localStorage.clear();
+export function Clear() {
+	localStorage.clear();
 }
 
-function Order(a,b) {
+export function Order(a, b) {
 	// Test de calcul de score
 	//console.log("score a ("+a[1]+") = "+(((20-a[3])*a[2])));
 	//console.log("score b ("+b[1]+") = "+((20-b[3])*b[2]));
 	return Score(a) - Score(b);
 }
 
-function Score(a) {
-	return ((20-a[3])*a[2]);
+export function Score(a) {
+	return ((20 - a[3]) * a[2]);
 }
 
 //Rend les joueurs par ordre de classement (indice 0 = plus faible; indice 1 < indice 2)
-function OrderClassement(Classement) {
-	return Classement.sort((a, b) => Order(a,b));
+export function OrderClassement(Classement) {
+	return Classement.sort((a, b) => Order(a, b));
 }
