6c62e9f7a3e3ee779795eec8f8bedc20ee052971
StarCobra <damienpoirot@yahoo.com>
2022-10-28 14:17:46 +0200

    feature : ajout classement général et bug fix

--- level_1.js
+++ level_1.js
@@ -15,10 +15,7 @@ let medium;
 let hard;
 let impossible;
 let range;
-let playNumber = document.querySelector("#playNumber");
 let difficulty;
-
-playNumber.innerHTML = "Saisissez un nombre entre 1 et 99.";
 
 function resetAllValeur() {
     nbEssais = 0;
@@ -34,18 +31,20 @@ function getEasy() {
     difficulty = 1;
     range = 99;
     nbEssaisMax = 40;
+
     resetAllValeur()
+
     return easy;
 }
 
 const inputEasy = document.querySelector("#easy");
 inputEasy.addEventListener("click", function (event) {
     getEasy();
-    let last = document.querySelector("#iDifficult");
-    last.style.display = "none";
-    let news = document.querySelector("#iPlay");
-    news.style.display = "block";
-    playNumber.innerHTML = "Saisissez un nombre entre 1 et 99.";
+
+    document.querySelector("#iDifficult").style.display = "none";
+    document.querySelector("#iTitreJeu").style.display = "none";
+    document.querySelector("#iPlay").style.display = "block";
+    document.querySelector("#playNumber").innerHTML = "Saisissez un nombre entre 1 et 99.";
 });
 
 function getMedium() {
@@ -54,18 +53,20 @@ function getMedium() {
     difficulty = 2;
     range = 99;
     nbEssaisMax = 20;
+
     resetAllValeur()
+
     return medium;
 }
 
 const inputMedium = document.querySelector("#medium");
 inputMedium.addEventListener("click", function (event) {
     getMedium();
-    let last = document.querySelector("#iDifficult");
-    last.style.display = "none";
-    let news = document.querySelector("#iPlay");
-    news.style.display = "block";
-    playNumber.innerHTML = "Saisissez un nombre entre 1 et 99.";
+
+    document.querySelector("#iDifficult").style.display = "none";
+    document.querySelector("#iTitreJeu").style.display = "none";
+    document.querySelector("#iPlay").style.display = "block";
+    document.querySelector("#playNumber").innerHTML = "Saisissez un nombre entre 1 et 99.";
 });
 
 function getHard() {
@@ -74,18 +75,20 @@ function getHard() {
     difficulty = 3;
     range = 99;
     nbEssaisMax = 10;
+
     resetAllValeur()
+
     return hard;
 }
 
 const inputHard = document.querySelector("#hard");
 inputHard.addEventListener("click", function (event) {
     getHard();
-    let last = document.querySelector("#iDifficult");
-    last.style.display = "none";
-    let news = document.querySelector("#iPlay");
-    news.style.display = "block";
-    playNumber.innerHTML = "Saisissez un nombre entre 1 et 99.";
+
+    document.querySelector("#iDifficult").style.display = "none";
+    document.querySelector("#iTitreJeu").style.display = "none";
+    document.querySelector("#iPlay").style.display = "block";
+    document.querySelector("#playNumber").innerHTML = "Saisissez un nombre entre 1 et 99.";
 });
 
 function getImpossible() {
@@ -94,18 +97,20 @@ function getImpossible() {
     difficulty = 4;
     range = 999;
     nbEssaisMax = 15;
+
     resetAllValeur();
+
     return impossible;
 }
 
 const inputImpossible = document.querySelector("#impossible");
 inputImpossible.addEventListener("click", function (event) {
     getImpossible();
-    let last = document.querySelector("#iDifficult");
-    last.style.display = "none";
-    let news = document.querySelector("#iPlay");
-    news.style.display = "block";
-    playNumber.innerHTML = "Saisissez un nombre entre 1 et 999.";
+
+    document.querySelector("#iDifficult").style.display = "none";
+    document.querySelector("#iTitreJeu").style.display = "none";
+    document.querySelector("#iPlay").style.display = "block";
+    document.querySelector("#playNumber").innerHTML = "Saisissez un nombre entre 1 et 999.";
 });
 
 function getValue() {
@@ -120,28 +125,27 @@ function Verification() {
 
         if (nbPropose > random) {
             document.querySelector("#response").innerHTML = nbPropose + " est supérieur au nombre Mystère !";
+
         } else if (nbPropose < random) {
             document.querySelector("#response").innerHTML = nbPropose + " est inférieur au nombre Mystère !";
+
         } else if (nbPropose == random) {
-            let news = document.querySelector("#iPlay");
-            news.style.display = "none";
-            let win = document.querySelector("#iWin");
-            win.style.display = "block";
+            document.querySelector("#iPlay").style.display = "none";
+            document.querySelector("#iWin").style.display = "block";
             document.querySelector("#numberMyst").innerHTML = random;
             document.querySelector("#theEssai").innerHTML = nbEssais;
             document.querySelector("#inputPseudo").value = "";
         }
 
         if (nbEssaisMax == nbEssais) {
-            let news = document.querySelector("#iPlay");
-            let lose = document.querySelector("#iLose");
-            news.style.display = "none";
-            lose.style.display = "block";
+            document.querySelector("#iPlay").style.display = "none"
+            document.querySelector("#iLose").style.display = "block";
             document.querySelector("#mysteriousNumber").innerHTML = random;
         }
     } else {
         alert("Votre nombre n'est pas compris entre 1 et " + range + ".");
     }
+
     document.querySelector("#essaiRestant").innerHTML = (nbEssaisMax - nbEssais);
     document.querySelector("#inputNumber").value = "";
 }
@@ -158,106 +162,136 @@ function recupPseudo() {
 
 const ButtonPseudo = document.querySelector("#submitPseudo");
 ButtonPseudo.addEventListener("click", function (event) {
-    let score = document.querySelector("#iScore");
-    score.style.display = "block";
-    let win = document.querySelector("#iWin");
-    win.style.display = "none";
+    document.querySelector("#iScore").style.display = "block";
+    document.querySelector("#iWin").style.display = "none";
+
     recupPseudo();
     SaveClassement(`${pseudo}`, `${nbEssais}`, `${difficulty}`);
+
     document.querySelector("#leaderboard").innerHTML = "";
+
     Classement();
 });
 
+function initClassement() {
+    document.querySelector("#leaderboard").innerHTML = "";
 
+    let first = LoadClassement();
+
+    OrderClassement(first);
+
+    for (let i = 0; i < 5; i++) {
+        if (first[i] != undefined) {
+            document.querySelector("#leaderboard").innerHTML += "<li>" + (i + 1) + "ème place : " + first[i][1] + " avec un score de " + Score(first[i]) + ".</li>";
+        }
+    }
+}
 
 function Classement() {
     let first = LoadClassement();
+
     OrderClassement(first);
-    console.log(OrderClassement);
+
     for (let i = 0; i < 10; i++) {
-        if(first[i] != undefined) {
-            document.querySelector("#leaderboard").innerHTML += "<li>" + (i + 1) + "ème place : " +first[i][1] + " avec un score de " + Score(first[i]) + ".</li>";
+        if (first[i] != undefined) {
+            document.querySelector("#leaderboard").innerHTML += "<li>" + (i + 1) + "ème place : " + first[i][1] + " avec un score de " + Score(first[i]) + ".</li>";
         }
     }
 }
 
 function ClassementGeneral() {
     let first = LoadClassement();
+
     OrderClassement(first);
-    console.log(OrderClassement);
+
     i = 1;
     first.forEach((u) => {
-        if(first[i] != undefined) {
-            document.querySelector("#leaderboard").innerHTML += "<li>" + i + "ème place : " +first[i][1] + " avec un score de " + Score(first[i]) + "</li>";
-            i ++;
+
+        if (first[i] != undefined) {
+            document.querySelector("#leaderboard").innerHTML += "<li>" + i + "ème place : " + first[i][1] + " avec un score de " + Score(first[i]) + "</li>";
+            i++;
         }
     })
 }
 
+window.addEventListener("load", initClassement);
+
+const ButtonLeaderboardHome = document.querySelector("#displayScoreHome");
+ButtonLeaderboardHome.addEventListener("click", function (event) {
+    document.querySelector("#iDifficultChoice").style.display = "none";
+    document.querySelector("#displayScoreHome").style.display = "none";
+    document.querySelector("#iTitreJeu").style.display = "none";
+    document.querySelector("#displayRetourHome").style.display = "block";
+    document.querySelector("#iDifficultTexte").innerHTML = "Voici tout le classement"
+
+    ClassementGeneral();
+});
+
+const ButtonRetourLeaderboardHome = document.querySelector("#displayRetourHome");
+ButtonRetourLeaderboardHome.addEventListener("click", function (event) {
+    document.querySelector("#iDifficultChoice").style.display = "flex";
+    document.querySelector("#displayScoreHome").style.display = "block";
+    document.querySelector("#iTitreJeu").style.display = "block";
+    document.querySelector("#displayRetourHome").style.display = "none";
+    document.querySelector("#iDifficultTexte").innerHTML = "Choisissez un niveau"
+
+    initClassement();
+});
 
 
 const leader = document.querySelector("#displayScore");
 leader.addEventListener("click", function () {
     document.querySelector("#leaderboard").innerHTML = "";
+
     Classement();
 })
 
 const ButtonScoreLose = document.querySelector("#displayScore");
 ButtonScoreLose.addEventListener("click", function (event) {
-    let score = document.querySelector("#iLose");
-    score.style.display = "none";
-    let difficult = document.querySelector("#iScore");
-    difficult.style.display = "block";
+    document.querySelector("#iLose").style.display = "none";
+    document.querySelector("#iScore").style.display = "block";
 });
 
 const ButtonRejouerLose = document.querySelector("#replayLose");
 ButtonRejouerLose.addEventListener("click", function (event) {
-    let score = document.querySelector("#iLose");
-    score.style.display = "none";
-    let difficult = document.querySelector("#iDifficult");
-    difficult.style.display = "block";
+    document.querySelector("#iLose").style.display = "none";
+    document.querySelector("#iDifficult").style.display = "block";
+    document.querySelector("#iTitreJeu").style.display = "block";
 });
 
 const ButtonRejouerScore = document.querySelector("#replayScore");
 ButtonRejouerScore.addEventListener("click", function (event) {
-    let score = document.querySelector("#iScore");
-    score.style.display = "none";
-    let difficult = document.querySelector("#iDifficult");
-    difficult.style.display = "block";
+    document.querySelector("#iScore").style.display = "none";
+    document.querySelector("#iDifficult").style.display = "block";
+    document.querySelector("#iTitreJeu").style.display = "block";
 });
 
 const ButtonReset = document.querySelector("#resetScore");
 ButtonReset.addEventListener("click", function (event) {
-    let score = document.querySelector("#iScore");
-    score.style.display = "none";
-    let difficult = document.querySelector("#iResetValidation");
-    difficult.style.display = "block";
+    document.querySelector("#iScore").style.display = "none";
+    document.querySelector("#iResetValidation").style.display = "block";
 });
 
 const ButtonResetValidation = document.querySelector("#resetValidation");
 ButtonResetValidation.addEventListener("click", function (event) {
-    let score = document.querySelector("#iScore");
-    score.style.display = "block";
-    let difficult = document.querySelector("#iResetValidation");
-    difficult.style.display = "none";
+    document.querySelector("#iScore").style.display = "block";
+    document.querySelector("#iResetValidation").style.display = "none";
+
     Clear();
+
     document.querySelector("#leaderboard").innerHTML = "";
 })
 
 const ButtonResetAnnulation = document.querySelector("#resetAnnulation");
 ButtonResetAnnulation.addEventListener("click", function (event) {
-    let score = document.querySelector("#iScore");
-    score.style.display = "block";
-    let difficult = document.querySelector("#iResetValidation");
-    difficult.style.display = "none";
+    document.querySelector("#iScore").style.display = "block";
+    document.querySelector("#iResetValidation").style.display = "none";
 })
 
 const ButtonFind = document.querySelector("#findPlayer");
 ButtonFind.addEventListener("click", function (event) {
-    let score = document.querySelector("#iScore");
-    score.style.display = "none";
-    let difficult = document.querySelector("#iUser");
-    difficult.style.display = "block";
+    document.querySelector("#iScore").style.display = "none";
+    document.querySelector("#iUser").style.display = "block";
 });
 
 const ButtonFindUser = document.querySelector("#submitFindUser");
@@ -268,10 +302,8 @@ ButtonFindUser.addEventListener("click", function (event) {
 
 const ButtonRetourScore = document.querySelector("#displayRetourScore");
 ButtonRetourScore.addEventListener("click", function (event) {
-    let score = document.querySelector("#iScore");
-    score.style.display = "block";
-    let difficult = document.querySelector("#iUser");
-    difficult.style.display = "none";
+    document.querySelector("#iScore").style.display = "block";
+    document.querySelector("#iUser").style.display = "none";
     document.querySelector("#inputFindUser").value = "";
     document.querySelector("#responseFindUser").innerHTML = "";
 });
