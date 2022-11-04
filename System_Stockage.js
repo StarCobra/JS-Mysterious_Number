03cf3402ed5f3e173b096f55dcd270953a552af1
Vigel <91243377+Vigelos0@users.noreply.github.com>
2022-11-04 18:55:46 +0100

    feature: Update. (#7)
    * Création de l'interface en HTML CSS
    
    * Système de Stockage et Loading de Classement
    
    NB : Ce système pour des raisons techniques se sert pas de document txt mais du LocalStorage (cad une forme de donnée de caché du navigateur).
    
    * Ajout du Stockage de config
    
    la raison pour laquelle une telle modification est dans cette branche est que cette branche gère les apsects de stockage divers, de fait cela a sa place.
    
    Concernant ses fonctionalités, cela sera décrit en détail dans le README ultérieurement.
    
    * Modification du README
    
    Mise à jour du README afin d'expliquer toute les fonctionnalités apportée par cette branche.
    
    * Mise à jour de mise en forme du README
    
    Il était peu lisible donc des améliorations de lisibilité ont été apportées.
    
    * MAJ README branche stockage
    
    Promis c'est le dernier.
    Réglage d'un oubli d'espace à un endroit.
    
    * feature: génération de nombre aléatoire.
    
    * feature: creation et test niveau 1
    
    feature: ajout de la validation via Enter key
    
    feature: ajout message de félicitations
    
    feature: ajout de la validation via Enter key
    
    * Ajout de plusieurs interfaces : niveau de difficulté et fin de partie. Amélioration du CSS.
    
    * n'affiche pas les autres sections lorsqu'on commence la partie
    
    * feature: mise a jour et test du niveau 1.
    
    * feature: update and fix.
    
    * feature: génération de nombre aléatoire.
    
    * feature: creation et test niveau 1
    
    feature: ajout de la validation via Enter key
    
    feature: ajout message de félicitations
    
    feature: ajout de la validation via Enter key
    
    * feature: mise a jour et reparation css.
    
    * feature: mise a jour et correction du niveau 1.
    
    * explication du fichier HTML
    
    * feature : ajout enregistrement classement et bug fix
    
    * feature: mise a jour et ajout classement.
    
    * feature: mise a jour
    
    * feature: jointure entre html/css et javascript
    
    * feature: ajout de nouvelle jointure entre html/css et javascript
    
    * feature : relation des boutons / affichage et correction de bugs
    
    * feature : new section on html. Clear leaderboard is operational
    
    * feature: ajout du top 10 joueurs.
    
    * feature: mise a jour classement
    
    * feature : validation reset leaderboard et bug fix
    
    * feature : ajout recherche de joueur et bug fix
    
    * feature: mise a jour du classement.
    
    * feature : amélioration du CSS et ajout de fichier .gif et du fond sonore
    
    * feature : ajout classement général et bug fix
    
    * feature : bug fix
    
    * feature : amélioration de la recherche du joueur et  du CSS pour le classement et les GIF
    
    * feature : amélioration du CSS, compatible sur smartphone et tablette
    
    Co-authored-by: leosix10 <leo.six1@etu.univ-lorraine.fr>
    Co-authored-by: StrixOfTheWeb <78547521+StrixOfTheWeb@users.noreply.github.com>
    Co-authored-by: StarCobra <damienpoirot@yahoo.com>
    Co-authored-by: Poirot Damien <113671617+StarCobra@users.noreply.github.com>
    Co-authored-by: Leo <91243419+leosix10@users.noreply.github.com>

---
+++ System_Stockage.js
@@ -0,0 +1,52 @@
+import { random } from './level_1.js';
+
+export function SaveClassement(Pseudo, NbEssais, difficulty) {
+	if(!difficulty) {difficulty = 2;}
+	if (LoadJoueur(Pseudo) == null) {
+		var Data = random + "|" + Pseudo + "|" + NbEssais + "|" + difficulty;
+		localStorage.setItem(Pseudo, Data);
+		return true;
+	} else {
+		return false;
+	}
+}
+
+export function LoadJoueur(Pseudo) {
+	return localStorage.getItem(Pseudo);
+}
+
+export function LoadIndex(Index) {
+	return LoadJoueur(localStorage.key(Index));
+}
+
+export function LoadClassement() {
+	var Classement = [];
+	var y = 0;
+	for (var i = 0; i < localStorage.length; i++) {
+		var temp = LoadIndex(i).split("|");
+		if (typeof temp[3] !== 'undefined') {
+			Classement[y++] = temp;
+		}
+	}
+	return Classement;
+}
+
+export function Clear() {
+	localStorage.clear();
+}
+
+export function Order(a, b) {
+	// Test de calcul de score
+	//console.log("score a ("+a[1]+") = "+(((20-a[3])*a[2])));
+	//console.log("score b ("+b[1]+") = "+((20-b[3])*b[2]));
+	return Score(b) - Score(a);
+}
+
+export function Score(a) {
+	return (((20 - a[2]) * a[3]));
+}
+
+//Rend les joueurs par ordre de classement (indice 0 = plus faible; indice 1 < indice 2)
+export function OrderClassement(Classement) {
+	return Classement.sort((a, b) => Order(a, b));
+}
---
+++ level_1.js
@@ -0,0 +1,309 @@
+'use_strict';
+
+import { SaveClassement, LoadJoueur, LoadIndex, LoadClassement, Clear, Order, Score, OrderClassement } from "./System_Stockage.js";
+
+function getRandomInt(max) {
+    return Math.floor((Math.random() * max) + 1);
+}
+
+export let random
+let nbEssais;
+let nbEssaisMax;
+let pseudo;
+let easy;
+let medium;
+let hard;
+let impossible;
+let range;
+let difficulty;
+
+function resetAllValeur() {
+    nbEssais = 0;
+    document.querySelector("#response").innerHTML = "";
+    document.querySelector("#inputFindUser").value = "";
+    document.querySelector("#essaiRestant").innerHTML = (nbEssaisMax - nbEssais);
+    document.querySelector("#responseFindUser").innerHTML = "";
+}
+
+function getEasy() {
+    easy = document.querySelector("#easy").value;
+    random = getRandomInt(98);
+    difficulty = 1;
+    range = 99;
+    nbEssaisMax = 40;
+
+    resetAllValeur()
+
+    return easy;
+}
+
+const inputEasy = document.querySelector("#easy");
+inputEasy.addEventListener("click", function (event) {
+    getEasy();
+
+    document.querySelector("#iDifficult").style.display = "none";
+    document.querySelector("#iTitreJeu").style.display = "none";
+    document.querySelector("#iPlay").style.display = "block";
+    document.querySelector("#playNumber").innerHTML = "Saisissez un nombre entre 1 et 99.";
+});
+
+function getMedium() {
+    medium = document.querySelector("#medium").value;
+    random = getRandomInt(98);
+    difficulty = 2;
+    range = 99;
+    nbEssaisMax = 20;
+
+    resetAllValeur()
+
+    return medium;
+}
+
+const inputMedium = document.querySelector("#medium");
+inputMedium.addEventListener("click", function (event) {
+    getMedium();
+
+    document.querySelector("#iDifficult").style.display = "none";
+    document.querySelector("#iTitreJeu").style.display = "none";
+    document.querySelector("#iPlay").style.display = "block";
+    document.querySelector("#playNumber").innerHTML = "Saisissez un nombre entre 1 et 99.";
+});
+
+function getHard() {
+    hard = document.querySelector("#hard").value;
+    random = getRandomInt(98);
+    difficulty = 3;
+    range = 99;
+    nbEssaisMax = 10;
+
+    resetAllValeur()
+
+    return hard;
+}
+
+const inputHard = document.querySelector("#hard");
+inputHard.addEventListener("click", function (event) {
+    getHard();
+
+    document.querySelector("#iDifficult").style.display = "none";
+    document.querySelector("#iTitreJeu").style.display = "none";
+    document.querySelector("#iPlay").style.display = "block";
+    document.querySelector("#playNumber").innerHTML = "Saisissez un nombre entre 1 et 99.";
+});
+
+function getImpossible() {
+    impossible = document.querySelector("#impossible").value;
+    random = getRandomInt(998);
+    difficulty = 4;
+    range = 999;
+    nbEssaisMax = 15;
+
+    resetAllValeur();
+
+    return impossible;
+}
+
+const inputImpossible = document.querySelector("#impossible");
+inputImpossible.addEventListener("click", function (event) {
+    getImpossible();
+
+    document.querySelector("#iDifficult").style.display = "none";
+    document.querySelector("#iTitreJeu").style.display = "none";
+    document.querySelector("#iPlay").style.display = "block";
+    document.querySelector("#playNumber").innerHTML = "Saisissez un nombre entre 1 et 999.";
+});
+
+function getValue() {
+    return document.querySelector("#inputNumber").value;
+}
+
+function Verification() {
+    let nbPropose = getValue();
+
+    if ((nbPropose <= range) && (nbPropose > 0)) {
+        nbEssais++;
+
+        if (nbPropose > random) {
+            document.querySelector("#response").innerHTML = nbPropose + " est supérieur au nombre Mystère !";
+
+        } else if (nbPropose < random) {
+            document.querySelector("#response").innerHTML = nbPropose + " est inférieur au nombre Mystère !";
+
+        } else if (nbPropose == random) {
+            document.querySelector("#iPlay").style.display = "none";
+            document.querySelector("#iWin").style.display = "block";
+            document.querySelector("#numberMyst").innerHTML = random;
+            document.querySelector("#theEssai").innerHTML = nbEssais;
+            document.querySelector("#inputPseudo").value = "";
+        }
+
+        if (nbEssaisMax == nbEssais) {
+            document.querySelector("#iPlay").style.display = "none"
+            document.querySelector("#iLose").style.display = "block";
+            document.querySelector("#mysteriousNumber").innerHTML = random;
+        }
+    } else {
+        alert("Votre nombre n'est pas compris entre 1 et " + range + ".");
+    }
+
+    document.querySelector("#essaiRestant").innerHTML = (nbEssaisMax - nbEssais);
+    document.querySelector("#inputNumber").value = "";
+}
+
+const ButtonPropose = document.querySelector("#submit");
+ButtonPropose.addEventListener("click", function (event) {
+    Verification();
+});
+
+function recupPseudo() {
+    pseudo = document.querySelector("#inputPseudo").value;
+    return pseudo;
+}
+
+const ButtonPseudo = document.querySelector("#submitPseudo");
+ButtonPseudo.addEventListener("click", function (event) {
+    document.querySelector("#iScore").style.display = "block";
+    document.querySelector("#iWin").style.display = "none";
+
+    recupPseudo();
+    SaveClassement(`${pseudo}`, `${nbEssais}`, `${difficulty}`);
+
+    document.querySelector("#leaderboard").innerHTML = "";
+
+    Classement();
+});
+
+function initClassement() {
+    document.querySelector("#leaderboard").innerHTML = "";
+
+    let first = LoadClassement();
+
+    OrderClassement(first);
+
+    for (let i = 0; i < 5; i++) {
+        if (first[i] != undefined) {
+            document.querySelector("#leaderboard").innerHTML += "<li><strong>" + first[i][1] + "</strong> avec un score de <strong>" + Score(first[i]) + "</strong></li>";
+        }
+    }
+}
+
+function Classement() {
+    let first = LoadClassement();
+
+    OrderClassement(first);
+
+    for (let i = 0; i < 10; i++) {
+        if (first[i] != undefined) {
+            document.querySelector("#leaderboard2").innerHTML += "<li><strong>" + first[i][1] + "</strong> avec un score de <strong>" + Score(first[i]) + "</strong></li>";
+        }
+    }
+}
+
+function ClassementGeneral() {
+    let first = LoadClassement();
+
+    OrderClassement(first);
+
+    let i = 1;
+    first.forEach((u) => {
+
+        if (first[i] != undefined) {
+            document.querySelector("#leaderboard").innerHTML += "<li><strong>" + first[i][1] + "</strong> avec un score de <strong>" + Score(first[i]) + "</strong></li>";
+            i++;
+        }
+    })
+}
+
+window.addEventListener("load", initClassement);
+
+const ButtonLeaderboardHome = document.querySelector("#displayScoreHome");
+ButtonLeaderboardHome.addEventListener("click", function (event) {
+    document.querySelector("#iDifficultChoice").style.display = "none";
+    document.querySelector("#displayScoreHome").style.display = "none";
+    document.querySelector("#iTitreJeu").style.display = "none";
+    document.querySelector("#displayRetourHome").style.display = "block";
+    document.querySelector("#iDifficultTexte").innerHTML = "Voici tout le classement"
+
+    ClassementGeneral();
+});
+
+const ButtonRetourLeaderboardHome = document.querySelector("#displayRetourHome");
+ButtonRetourLeaderboardHome.addEventListener("click", function (event) {
+    document.querySelector("#iDifficultChoice").style.display = "flex";
+    document.querySelector("#displayScoreHome").style.display = "block";
+    document.querySelector("#iTitreJeu").style.display = "block";
+    document.querySelector("#displayRetourHome").style.display = "none";
+    document.querySelector("#iDifficultTexte").innerHTML = "Choisissez un niveau"
+
+    initClassement();
+});
+
+
+const leader = document.querySelector("#displayScore");
+leader.addEventListener("click", function () {
+    document.querySelector("#leaderboard").innerHTML = "";
+
+    Classement();
+})
+
+const ButtonScoreLose = document.querySelector("#displayScore");
+ButtonScoreLose.addEventListener("click", function (event) {
+    document.querySelector("#iLose").style.display = "none";
+    document.querySelector("#iScore").style.display = "block";
+});
+
+const ButtonRejouerLose = document.querySelector("#replayLose");
+ButtonRejouerLose.addEventListener("click", function (event) {
+    document.querySelector("#iLose").style.display = "none";
+    document.querySelector("#iDifficult").style.display = "block";
+    document.querySelector("#iTitreJeu").style.display = "block";
+});
+
+const ButtonRejouerScore = document.querySelector("#replayScore");
+ButtonRejouerScore.addEventListener("click", function (event) {
+    document.querySelector("#iScore").style.display = "none";
+    document.querySelector("#iDifficult").style.display = "block";
+    document.querySelector("#iTitreJeu").style.display = "block";
+});
+
+const ButtonReset = document.querySelector("#resetScore");
+ButtonReset.addEventListener("click", function (event) {
+    document.querySelector("#iScore").style.display = "none";
+    document.querySelector("#iResetValidation").style.display = "block";
+});
+
+const ButtonResetValidation = document.querySelector("#resetValidation");
+ButtonResetValidation.addEventListener("click", function (event) {
+    document.querySelector("#iScore").style.display = "block";
+    document.querySelector("#iResetValidation").style.display = "none";
+
+    Clear();
+
+    document.querySelector("#leaderboard2").innerHTML = "";
+})
+
+const ButtonResetAnnulation = document.querySelector("#resetAnnulation");
+ButtonResetAnnulation.addEventListener("click", function (event) {
+    document.querySelector("#iScore").style.display = "block";
+    document.querySelector("#iResetValidation").style.display = "none";
+})
+
+const ButtonFind = document.querySelector("#findPlayer");
+ButtonFind.addEventListener("click", function (event) {
+    document.querySelector("#iScore").style.display = "none";
+    document.querySelector("#iUser").style.display = "block";
+});
+
+const ButtonFindUser = document.querySelector("#submitFindUser");
+ButtonFindUser.addEventListener("click", function (event) {
+    let user = document.querySelector("#inputFindUser").value;
+    document.querySelector("#responseFindUser").innerHTML = LoadJoueur(user);
+});
+
+const ButtonRetourScore = document.querySelector("#displayRetourScore");
+ButtonRetourScore.addEventListener("click", function (event) {
+    document.querySelector("#iScore").style.display = "block";
+    document.querySelector("#iUser").style.display = "none";
+    document.querySelector("#inputFindUser").value = "";
+    document.querySelector("#responseFindUser").innerHTML = "";
+});
